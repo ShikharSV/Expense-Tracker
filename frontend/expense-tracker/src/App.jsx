6937e738eab12432d6697ab5789a2f72b4109423
Shikhar Singh <shikhar.s@virtualizetechnologies.com>
2025-07-23 07:38:17 +0530

    Added app routes

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+ } from 'react-router-dom'
 
+ import Login from "./pages/auth/Login";
+ import SignUp from "./pages/auth/Signup";
+//  import Home from "./pages/dashboard/Home"
+ import Home from './pages/dashboard/Home';
+ import Expense from "./pages/dashboard/Expense"
+ import Income from "./pages/dashboard/Income"
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Root />}/>
+          <Route path='/login' exact element={<Login />}/>
+          <Route path='/signup' exact element={<SignUp/>}/>
+          <Route path='/dashboard' exact element={<Home/>}/>
+          <Route path='/income' exact element={<Income/>}/>
+          <Route path='/expense' exact element={<Expense/>}/>
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
+const Root = () =>{
+  const isAuthenticated = !!localStorage.getItem("token");
+  return isAuthenticated ? (
+    <Navigate to="/dashboard"/>
+  ) : (
+    <Navigate to="/login"/>
+  )
+}
 export default App
